634537df03c39edecc8363e43ea1a1436cf07ee7
Ben Leivian <ben@drawbackwards.com>
2015-01-30 13:47:38 -0700

    Underscores starter theme.

---
+++ src/content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Forward
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php forward_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'forward' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php forward_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
