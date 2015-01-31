91b6fdc5201f4f2b5c8ffebb0fe993767beccb4f
Ben Leivian <ben@drawbackwards.com>
2015-01-30 17:19:00 -0700

    Base modifications.

--- gulpfile.js
+++ gulpfile.js
@@ -166,7 +166,7 @@ gulp.task('bower-normalize', function() {
 // ==== WATCH & RELOAD ==== //
 gulp.task('browser-sync', function() {
     browserSync({
-        proxy: "design.org"
+        proxy: "forward-test-site.local"
     });
 });
 
