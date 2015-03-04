3226f1c3dbeeb1dc6a7858cdaf68fa1c0f9a8269
Ben Leivian <ben@drawbackwards.com>
2015-03-03 17:38:03 -0700

    Styles for navigation & fixed header bar on mobile (partial)

--- src/header.php
+++ src/header.php
@@ -27,13 +27,12 @@
 				<h1 class="site-title"><a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
 			</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<div class="container">
+					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+				</div>
+			</nav><!-- #site-navigation -->
 		</div>
 	</header><!-- #masthead -->
-
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<div class="container">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</div>
-	</nav><!-- #site-navigation -->
 
 	<div id="content" class="site-content">
