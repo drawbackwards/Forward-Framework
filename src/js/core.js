40424a3e3f659dcbffb015aa7dd258e901472f34
Ben Leivian <ben@drawbackwards.com>
2015-03-10 17:36:22 -0700

    Mobile menu functionality (toggle & fit viewport).

--- src/js/core.js
+++ src/js/core.js
@@ -1,1 +1,30 @@
-/* Custom functions */
+(function ($) {
+
+	$(document).ready(function() {
+
+		// Show mobile menu
+		$('#mobile-menu-switch .toggle').click(function(event) {
+			event.stopPropagation();
+			$(this).toggleClass('on');
+			$('#site-navigation').toggle();
+			setMobileMenuHeight();
+			event.preventDefault();
+		});
+	});
+
+	$(window).resize(function() {
+	    setMobileMenuHeight();
+	});
+
+	// Resize mobile menu to 
+	function setMobileMenuHeight() {
+		var headerHeight = $('#masthead').outerHeight();
+		var windowHeight = window.innerHeight;
+		var menuOffset = headerHeight;
+		var menuHeight = windowHeight - menuOffset;
+
+		$("#site-navigation").css("height", menuHeight);
+		// .css("top", menuOffset);
+	}
+
+})(jQuery);
