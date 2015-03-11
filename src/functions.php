ef505bfc09657bffb512ecb11c27adf69aecfa76
Ben Leivian <ben@drawbackwards.com>
2015-03-10 17:35:27 -0700

    Integrate front-end scripts.

--- src/functions.php
+++ src/functions.php
@@ -102,6 +102,25 @@ add_action( 'widgets_init', 'forward_widgets_init' );
 function forward_scripts() {
 	wp_enqueue_style( 'forward-style', get_stylesheet_uri() );
 
+	// Front-end scripts
+	if ( !is_admin() ) {
+
+	  // Load minified scripts if debug mode is off
+	  if ( WP_DEBUG === true ) {
+	    $suffix = '';
+	  } else {
+	    $suffix = '.min';
+	  }
+
+	  // Load theme-specific JavaScript with versioning based on last modified time; http://www.ericmmartin.com/5-tips-for-using-jquery-with-wordpress/
+	  wp_enqueue_script( 'forward-js-core', get_stylesheet_directory_uri() . '/js/core' . $suffix . '.js', array( 'jquery' ), filemtime( get_template_directory() . '/js/core' . $suffix . '.js' ), true );
+
+	  // Conditionally load another script
+	  // if ( is_singular() ) {
+	  //   wp_enqueue_script( 'my-theme-extras', get_stylesheet_directory_uri() . '/js/extras' . $suffix . '.js', array( 'jquery' ), filemtime( get_template_directory() . '/js/extras' . $suffix . '.js' ), true );
+	  // }
+	}
+
 	// wp_enqueue_script( 'forward-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	// wp_enqueue_script( 'forward-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
