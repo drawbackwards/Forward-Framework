bafe94b23147aaff21605ffbf13cba4c8f9c1179
Ben Leivian <ben@drawbackwards.com>
2015-06-05 18:57:14 +0000

    Metag to disable IE compatibility mode.

--- src/header.php
+++ src/header.php
@@ -10,6 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
