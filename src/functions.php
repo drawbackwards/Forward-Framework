4097e7d93dfab409114f73f63c0a3852915dec49
Ben Leivian <ben@drawbackwards.com>
2015-07-27 17:10:40 -0700

    Add theme prefix to font function.

--- src/functions.php
+++ src/functions.php
@@ -6,14 +6,17 @@
  */
 
 
-if ( ! function_exists( 'google_fonts' ) ) :
+if ( ! function_exists( 'forward_google_fonts' ) ) :
 /**
  * Adds google font support.
  * 
  */
-function google_fonts() {
+function forward_google_fonts() {
 	$query_args = array(
 		'family' => 'Source+Sans+Pro:200,300,400,600',
+
+		// Here's an example for changing fonts.
+		// 
 		// 'family' => 'Open+Sans:400,700|Oswald:700',
 		// 'subset' => 'latin,latin-ext',
 	);
@@ -22,8 +25,8 @@ function google_fonts() {
 
 	wp_enqueue_style('source-sans');
 }
-endif; // google_fonts
-add_action('wp_enqueue_scripts', 'google_fonts');
+endif; // forward_google_fonts
+add_action('wp_enqueue_scripts', 'forward_google_fonts');
 
 /**
  * Set the content width based on the theme's design and stylesheet.
