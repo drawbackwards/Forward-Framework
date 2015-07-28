b3beb8c700003ac530cb394ce3c94bf1f26f23bb
Ben Leivian <ben@drawbackwards.com>
2015-07-27 17:14:43 -0700

    Disable unused customizer sections.

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -13,7 +13,15 @@
 function forward_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	// Optionally disable individual customizer options.
+	// 
+	// $wp_customize->remove_control('blogdescription');
+
+	// Optionally disable customizer sections.
+	// 
+	$wp_customize->remove_section('background_image');
+	$wp_customize->remove_section('colors');
 }
 add_action( 'customize_register', 'forward_customize_register' );
 
