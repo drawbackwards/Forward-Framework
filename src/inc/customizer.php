6befd1e8b686fbbc901135459d362234228c548a
Ben Leivian <ben@drawbackwards.com>
2015-12-03 20:15:10 -0700

    Remove reference to customizer.js.

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -15,20 +15,12 @@ function forward_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 
 	// Optionally disable individual customizer options.
-	// 
+	//
 	// $wp_customize->remove_control('blogdescription');
 
 	// Optionally disable customizer sections.
-	// 
+	//
 	$wp_customize->remove_section('background_image');
 	$wp_customize->remove_section('colors');
 }
 add_action( 'customize_register', 'forward_customize_register' );
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function forward_customize_preview_js() {
-	wp_enqueue_script( 'forward_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
-}
-add_action( 'customize_preview_init', 'forward_customize_preview_js' );
